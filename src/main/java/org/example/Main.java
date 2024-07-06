e7211173348787aed56975e68cf5983c1eacfbe4
Xigwa <bonya377@gmail.com>
2024-07-06 21:45:33 +0200

    add git to projekt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         MusicFile musicFile = new MusicFile();
         // Относительный путь к файлу в папке resources
         musicFile.playMusic("Music/Teya_Dora_Dzanum.mp3");
+        //
     }
 }
